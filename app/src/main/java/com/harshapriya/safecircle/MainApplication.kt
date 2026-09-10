de8c63854c73c7d664ec7bcac138497ff8b6ab42
Harsha Priya Ganapathy <121929535+harshapriyag123@users.noreply.github.com>
2026-09-09 21:47:34 -0500

    feat: bootstrap capsule retention and offline sync

--- app/src/main/java/com/harshapriya/safecircle/MainApplication.kt
+++ app/src/main/java/com/harshapriya/safecircle/MainApplication.kt
@@ -2,7 +2,9 @@ package com.harshapriya.safecircle
 
 import android.app.Application
 import android.util.Log
+import com.harshapriya.safecircle.background.SyncScheduler
 import com.harshapriya.safecircle.billing.SubscriptionManager
+import com.harshapriya.safecircle.security.SafetyCapsuleStore
 import com.harshapriya.safecircle.data.Constants
 import com.revenuecat.purchases.LogLevel
 import com.revenuecat.purchases.Purchases
@@ -27,5 +29,7 @@ class MainApplication : Application() {
             SubscriptionManager.update(it)
         }
         SubscriptionManager.refresh()
+        SafetyCapsuleStore(this).purgeExpired()
+        SyncScheduler.ensureScheduled(this)
     }
 }
